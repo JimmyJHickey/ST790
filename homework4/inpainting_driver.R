3af4ddad412744b2e3eb7a3adff71ca24f99ce0b
jimmyjhickey <jimmyjhickey@gmail.com>
2020-03-05 15:40:11 -0500

    Reading and displaying image

--- homework4/inpainting_driver.R
+++ homework4/inpainting_driver.R
@@ -6,3 +6,13 @@
 # Run inpainting code
 #
 ###
+
+
+ruscha = read.csv("~/git/jhickeyST790/homework4/data/Ruscha_in.csv")
+cox = read.csv("~/git/jhickeyST790/homework4/data/gertrude_cox_in.csv")
+
+r_mat = as.matrix(ruscha)
+image(r_mat)
+
+c_mat = as.matrix(cox)
+image(c_mat)
