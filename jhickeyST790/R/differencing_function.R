1c7098150e6c671ec71845ee22b53307eab9da87
jimmyjhickey <jimmyjhickey@gmail.com>
2020-03-05 15:39:50 -0500

    Added negative functionality

--- jhickeyST790/R/differencing_function.R
+++ jhickeyST790/R/differencing_function.R
@@ -2,11 +2,17 @@
 #'
 #' @param k order of the differencing matrix
 #' @param n Number of time points
+#' @param negative Flip signs, row one will look lie 1 -1 0 ... 0 (default: FALSE)
 #' @return kth order differencing matrix
-myGetDkn <- function(k, n)
+myGetDkn <- function(k, n, negative=FALSE)
 {
   library(Matrix)
-  # create D1n
+  neg = negative
+
+  ii = 1 + (!negative) * (-2)
+  iiplusone = -ii
+
+   # create D1n
   if (k==1)
   {
     D1 = matrix(nrow = n-1, ncol = n)
@@ -15,13 +21,13 @@ myGetDkn <- function(k, n)
     for(i in 1:n-1)
     {
       D1[i,] = zeros_matrix
-      D1[i,i] = -1
-      D1[i, i+1] = 1
+      D1[i,i] = ii
+      D1[i, i+1] = iiplusone
     } # for
     return(Matrix(D1, sparse = TRUE))
   } # endif
 
-  return(myGetDkn(k=1, n=n - k + 1) %*% myGetDkn(k=k-1, n=n))
+  return(myGetDkn(k=1, n=n - k + 1, neg) %*% myGetDkn(k=k-1, n=n, neg))
 
 }
 
