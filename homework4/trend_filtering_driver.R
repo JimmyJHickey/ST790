7264014dc29fd32d709dc3c4140122159ddc50dc
jimmyjhickey <jimmyjhickey@gmail.com>
2020-03-05 19:56:02 -0500

    Added trend filtering

--- homework4/trend_filtering_driver.R
+++ homework4/trend_filtering_driver.R
@@ -6,3 +6,37 @@
 # Run l1 trend filtering code
 #
 ###
+
+# apple closing prices 2018-2020
+library(tseries)
+library(ggplot2)
+
+apple_data = get.hist.quote( instrument = "AAPL",
+                             start = "2018-01-01",
+                             end = "2020-01-01",
+                             quote="Close")
+
+times = time(apple_data)
+time_stock = cbind(times, as.data.frame(apple_data))
+
+
+apple_plots = function(k, lambda)
+ {
+  n = nrow(time_stock)
+
+  y=time_stock[,2]
+
+  l1_fit = myTrendFilter(y, k, lambda)$x[1:n]
+
+  aapl_df = data.frame(cbind(time_stock, l1_fit))
+
+  ggplot(data = aapl_df, aes(x = times)) +
+   geom_point(aes(y = Close), size = 0.1) +
+   geom_line(aes(y = l1_fit)) +
+   ggtitle(paste("Apple Stock Prices: ", expression(lambda), " =", lambda , ", k = ", k ))
+ }
+
+apple_plots(k = 3, lambda = 0.5)
+apple_plots(k = 10, lambda = 10)
+apple_plots(k = 3, lambda = 10)
+
--- jhickeyST790/R/l1_trend_filtering.R
+++ jhickeyST790/R/l1_trend_filtering.R
@@ -5,5 +5,62 @@
 #' @param lambda regularization parameter
 #' @export
 myTrendFilter <- function(y, k, lambda) {
+ library(gurobi)
+
+ n = length(y)
+
+ Dk = myGetDkn(k = k, n = n)
+
+ nrow_d = nrow(Dk)
+
+ neg_id_d = -1 * Diagonal(nrow_d)
+ pos_id_d = Diagonal(nrow_d)
+ half_id_n = 0.5 * Diagonal(n)
+
+ zero_d_n = Matrix(0, nrow=nrow_d, ncol = n, sparse=TRUE)
+ zero_d_d = Matrix(0, nrow=nrow_d, ncol = nrow_d, sparse=TRUE)
+ zero_n_d = Matrix(0, nrow=n, ncol = nrow_d, sparse=TRUE)
+ zero_n_n = Matrix(0, nrow=n, ncol = n, sparse=TRUE)
+
+ lambda_d = rep(lambda, nrow_d)
+
+ linear = c(-1 * t(y), lambda_d, lambda_d)
+
+ quadratic = rbind(
+                   cbind(half_id_n, zero_n_d, zero_n_d),
+                   cbind(zero_d_n, zero_d_d, zero_d_d),
+                   cbind(zero_d_n, zero_d_d, zero_d_d)
+                  )
+
+ equality_constraints = cbind(Dk, neg_id_d, pos_id_d)
+ inequality_constraints = rbind(
+                               cbind(zero_d_n, neg_id_d, zero_d_d),
+                               cbind(zero_d_n, zero_d_d, neg_id_d)
+                               )
+
+ model = list()
+
+
+ model$A = rbind(equality_constraints, inequality_constraints)
+ model$Q = quadratic
+ model$obj = linear
+ model$rhs = c(
+               rep(0, nrow_d),
+               rep(0, nrow_d),
+               rep(0, nrow_d)
+              )
+ model$sense = c(
+                 rep("=", nrow_d),
+                 rep("<=", nrow_d),
+                 rep("<=", nrow_d)
+                )
+
+ cat(dim( model$A))
+ cat(" ")
+ cat(dim( model$Q))
+
+ result = gurobi(model)
+
+ return(result)
 
 }
